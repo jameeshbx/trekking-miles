b843c72012b074abe3cfdc5ab50c1710c66bc32b
VaisakhBuyexchange <vaishak@buyexchange.in>
2025-03-24 10:15:29 +0000

    Resolve "FE: Features Section"

---
+++ app/data/featured.ts
@@ -0,0 +1,39 @@
+export const featuresData = [
+    {
+      imagePath: "/img/Vector (1).svg",
+      title: "Smart Itinerary Builder",
+      description: "Create, edit & share travel plans in minutes.",
+    },
+    {
+      imagePath: "/img/Vector (2).svg",
+      title: "Easy Booking Management",
+      description: "Book hotels, transport & activities in one place.",
+    },
+    {
+      imagePath: "/img/Vector.svg",
+      title: "Secure Payments & Invoicing",
+      description: "Accept payments, track transactions & generate invoices.",
+    },
+    {
+      imagePath: "/img/Group.svg",
+      title: "DMC & Supplier Collaboration",
+      description: "Connect with DMCs, compare prices & finalize deals.",
+    },
+    {
+      imagePath: "/img/Group (1).svg",
+      title: "Mobile-Friendly Access",
+      description: "Manage everything on the go with a simple interface.",
+    },
+    {
+      imagePath: "/img/Clip path group.svg",
+      title: "Customized Templates",
+      description: "Create, save tailored itinerary templates for quick travel plans.",
+    },
+    {
+      imagePath: "/img/Group (1).svg",
+      title: "Live Dashboard & Reports",
+      description: "Track bookings, payments & agency performance.",
+    },
+  ]
+  
+  
