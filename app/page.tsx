59f353dd903b1d3ac0da0d5877c2dff213ae3c18
AnandBuyExchange <anand@buyexchange.in>
2025-03-25 07:40:19 +0000

    Resolve "FE: Create Footer Section"

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+
+import Footer from "./components/footer";
 import Navbar from "./components/topBar";
 import Feature from "./components/featured-section";
 
 export default function Home() {
   return (
     <div>
+      
+      
       <Navbar />
       <Feature />
-      
+      <Footer/>
       
     </div>
   );
