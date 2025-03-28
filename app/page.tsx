5894dc2f792718446d8e9e045b2be090df3a344f
Nisha <nishamolmanayil152@gmail.com>
2025-03-28 17:22:06 +0530

    10-contactSection first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,8 @@ import Navbar from "./components/topBar";
 import Feature from "./components/featured-section";
 import Pricing from "./components/pricing-section";
 import Herosection from "./components/hero-section";
-<<<<<<< HEAD
 import ContactSection from "./components/contactSection";
-=======
 import FAQSection from "./components/faq-section";
->>>>>>> 6beb8f0d0b66e3d3af3ee2fb4aac19606e2c0a6f
 
 export default function Home() {
   return (
@@ -19,13 +16,10 @@ export default function Home() {
       <Herosection/>
       <Feature />
       <Pricing />
-<<<<<<< HEAD
       <div className="relative py-16">
         <ContactSection/>
       </div>
-=======
       <FAQSection/>
->>>>>>> 6beb8f0d0b66e3d3af3ee2fb4aac19606e2c0a6f
       <Footer/>
       
     </div>
